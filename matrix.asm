822db82872fad8af9a3b36c846f98e33c0806da6
ChiChaChai <chatchaishaetan@gmail.com>
2018-02-20 21:20:36 +0700

    implement update matrix

--- matrix.asm
+++ matrix.asm
@@ -2,88 +2,77 @@
     ; .stack  0100h
 
     .data   
-_Matrix    struc
-    x   db  10h
-    y   db  10h
-    len db  10h  
-    _Matrix ends
-
-matrix      _Matrix    <5>
-temp_matrix     _Matrix <0>
-    i   db  0
+i           db  0
 
 num_Matrix  db  5h
+matrixY     db  80  dup(0)
+
 
     .code
     
     org     0100h
     
 main:
-    
-
-    ; lea     bx,matrix[0]
-    ; mov     [bx],8h
-    ; inc     bx
-    ; mov     [bx],8h
-    ; inc     bx
-    ; mov     [bx],3h
-    
-
-    
-    ; lea     bx,matrix[0]
-    ; add     bx,1h
-    ; mov     cx,[bx]
-    ; call    test_print
-    ; ret
-
-
+    mov     dh,5
+    mov     dl,5
+    call    printCharAt
     ret
 init_all_matrix:
     mov     cx,0h
     mov     cl,num_Matrix                   ;set number of for loop running
 loop_init_all_matrix:
-
-    call    init_matrix
-
-    ; lea     bx,matrix.x
-
-        
+    mov     dh,64h                          ;random number (100,150)
+    mov     dl,96h
+    call    random_number
+    
+    mov     si,cx
+    mov     matrixY[si],dl                  ;store random number to matrixY
+            
     loop    loop_init_all_matrix 
 
-    ret
-
-test_print:
-    push    bx
-    mov     dx,bx
-    mov     ah,0ah
-    mov     al,'P'
-    mov     bh,0h
-
-    int     10h
-
-    pop     bx
-    ret
-init_matrix:
-    mov     dh,0h                   ;random number between 0 to 80
-    mov     dl,50h
+    mov     dh,4h
+    mov     dl,6h
     call    random_number
-
-    mov     [temp_matrix.x],dl      ;store random(0,80) to temp_matrix.x
-
-
-    mov     dh,64h
-    mov     dh,96h
-    call    random_number           ;random number between 100 to 150
-
-    mov     [temp_matrix.y],dl      ;store random(100,150) to temp_matrix.y
-
-    mov     dh,0ah              
-    mov     dl,14h
-    call    random_number           ;random number between 10 to 20
-
-    mov     [temp_matrix.len],dl    ;store random(10,20) to temp_matrix.len
     
+    mov     si,cx
+    mov     matrixY[si],dl
+
     ret
+
+update_all_matrix:
+    mov     cx,0h
+    mov     cl,[num_Matrix]
+loop_update_all_matrix:
+    mov     si,cx                           ;make si to index point "i"
+
+    cmp     matrixY[si],150                 ;if matrix[i] < 150
+    jl      ifLessThan150
+    jge     ifNotLessThan150
+endIfLessThan150:
+
+
+    cmp     matrixY[si],40                  ;if matrix[i] < 40
+    jl      ifLessThan40
+    je      ifEqual40
+endIfLessThan40:
+
+    loop    loop_update_all_matrix
+    ret
+
+ifLessThan150:
+    inc     matrixY[si]                     ;matrix[i]++;
+    jmp     endIfLessThan150
+ifNotLessThan150:
+    mov     matrixY[si],0h                  ;matrix[i]=0
+    jmp     endIfLessThan150
+
+ifLessThan40:
+    ;PRINT_FLOW_MATRIX
+
+    jmp     endIfLessThan40
+ifEqual40:
+    mov     matrixY[si],40
+    jmp     endIfLessThan40
 
 random_number:              ;random number from dh to dl
     push    ax              ;backup value ax
@@ -114,7 +103,30 @@ random_number:              ;random number from dh to dl
     pop     ax
 
     ret
+printCharAt:                ;print random character at position (dh = row,dl = column)
+                            ;with color ( bl = color code)
+    push    ax
+    push    cx
 
+    mov     ah,02h                          
+    mov     bh,0h                           ;move cursor
+    int     10h                             ;call move cursor interrupt                                     
+
+    mov     dh,33
+    mov     dl,126
+    call    random_number                   ;random character decimal number
+
+
+    mov     ah,09h                          ;print O
+    mov     al,dl                           ;store character
+    mov     bh,0h                         
+    mov     cx,1h
+    int     10h                             ;call print character interrupt
+
+    pop     cx
+    pop     ax
+
+    ret
 exit:
     ret
 
--- run_asm.bat
+++ run_asm.bat
@@ -7,6 +7,6 @@ del MATRIX.MAP
 tasm matrix.asm
 
 if EXIST "MATRIX.OBJ"  tlink MATRIX.OBJ /t
-REM if EXIST "MATRIX.COM"  cls  
+if EXIST "MATRIX.COM"  cls  
 if EXIST "MATRIX.COM" MATRIX.COM
 echo.
